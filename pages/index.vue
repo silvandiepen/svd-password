2cab9e7924cbcb1a9fd1b5e76107ad13fd5245f8
Sil <sil@matise.nl>
2018-05-04 12:46:33 +0200

    working pw and copy

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,23 @@
   <row center>
     <column medium="two-third">
       <section class="view basic">
-        <div class="password" v-if="password">
-          <div :class="[{ copied: isCopied }, 'overlay']" @mouseleave="isCopied = false">
-            {{isCopied}}
-            <button class="button--rounded" v-on:click="generatePassword">&#8635; Refresh</button>
-            <button class="button--rounded" v-clipboard:copy="password" v-clipboard:success="isCopied = true">&#x1f4cb; Copy to clipboard</button>
-          </div>
+        <div class="password" v-if="password">   
           {{password}}
+          <div :class="['overlay', isCopied ? 'copied' : '']" @mouseleave="onCopyLeave">
+            <button class="button--rounded" v-on:click="generatePassword">&#8635; Refresh</button>
+            <button class="button--rounded" v-clipboard:copy="password" v-clipboard:success="onCopy">&#x1f4cb; Copy to clipboard</button>
+            <h3 v-if="isCopied">Woohoo, copied!</h3>
+          </div>
         </div>
       </section>
       <section class="settings basic">
         <row center medium="1:3">
-
           <column medium="half">
             <fieldset>
               <label>length</label>
               <span class="range">
                 <span class="range__amount">{{settings.totalChars}}</span>
-                <input class="range__slider" type="range" min="0" max="256" v-model="settings.length">
+                <input class="range__slider" type="range" min="0" max="256" v-model="settings.totalChars">
               </span>
             </fieldset>
             <fieldset>
@@ -28,6 +27,7 @@
                 <input type="checkbox" id="settings_letters" v-model="settings.letters">
                 <label for="settings_letters"></label>
               </div>
+
               <br />
 
               <label>numbers</label>
@@ -81,7 +81,6 @@
               </div>
             </fieldset>
           </column>
-
         </row>
       </section>
     </column>
@@ -157,7 +156,12 @@ export default {
     };
   },
   methods: {
-
+    onCopy: function(e){
+      this.isCopied = true; 
+    },
+    onCopyLeave: function(e){
+      this.isCopied = false; 
+    },
     randomNumber: function getRandomInt(max) {
       let min = 0;
       min = Math.ceil(min);
@@ -327,7 +331,7 @@ export default {
     justify-content: center;
     opacity: 0;
     transform: translateY(-100%);
-    transition: transform 0 0.3s, opacity 0.3s;
+    transition: transform 0 0.3s, opacity 0.3s, background-color .3s;
     button {
       transform: translateY(-100vh);
       &:nth-child(1) {
@@ -338,7 +342,28 @@ export default {
       }
     }
     &.copied{
-      background-color: color(Green);
+      background-color: color(Green,.5);
+      &,&:hover{
+        button{
+        transition: transform .2s; 
+         transform: translateY(-100vh);
+        }
+      }
+      h3{
+        position: absolute; left: 50%; top: 50%; transform: translate(-50%,200%); width: 100%; text-align: center; color: white; 
+        text-shadow: .1rem .1rem .5rem color(Black,.25); 
+        animation: comeUp .3s .5s forwards;
+        @at-root{
+          @keyframes comeUp {
+            0%{
+              transform: translate(-50%,200%);
+            }            
+            100%{
+              transform: translate(-50%,-50%);
+            }
+          }
+        }
+      }
     }
   }
   &:hover {
@@ -358,15 +383,6 @@ export default {
     }
   }
 }
-.settings {
-  &__column {
-    // display: flex;
-    // flex-wrap: wrap;
-    // align-items: flex-start;
-    // justify-content: flex-start;
-  }
-}
-
 fieldset {
   border: 2px solid color(Offwhite);
   padding: 1rem;
@@ -393,7 +409,7 @@ fieldset {
 }
 .error {
   background-color: color(Red, 0.2);
-  border: 1px solid color(Red, 0.5);
+  border:     1px solid color(Red, 0.5);
   padding: 1em;
   border-radius: 3px;
   font-size: 12px;
